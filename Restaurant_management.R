658538b8746b3870eda346c777b110a62baeb15d
cdelaguardia <cdelaguardia1987@gmail.com>
2023-07-26 13:30:19 -0600

    Creating file. Calculating posible meal combinations

---
+++ Restaurant_management.R
@@ -0,0 +1,20 @@
+#Use the information below to answer the following five questions.
+
+#A restaurant manager wants to advertise that his lunch special offers 
+#enough choices to eat different meals every day of the year. 
+#He doesn't think his current special actually allows that number of 
+#choices, but wants to change his special if needed to allow at least 
+#365 choices.
+
+#A meal at the restaurant includes 1 entree, 2 sides, and 1 drink. 
+#He currently offers a choice of 1 entree from a list of 6 options, 
+#a choice of 2 different sides from a list of 6 options, and a choice 
+#of 1 drink from a list of 2 options.
+
+#How many meal combinations are possible with the current menu?
+
+sides_options <- 6
+entrees_options <- 6
+drinks_options <- 2
+
+meal_combinations <- choose(entrees_options, 1) * choose(sides_options, 2) * choose(drinks_options, 1)
