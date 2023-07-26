0a2e5d02d668ce3b82757f85b893baefc4c266cf
cdelaguardia <cdelaguardia1987@gmail.com>
2023-07-26 14:37:50 -0600

    Writing to a function that takes a number of entree choices and returns the number of meal combinations

--- Restaurant_management.R
+++ Restaurant_management.R
@@ -24,10 +24,46 @@ drinks_choices <- 1
 meal_combinations <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
 
 #How many combinations are possible if he expands his original special to 3 drink options?
+sides_options <- 6
+entrees_options <- 6
 drinks_options <- 3
-meal_combinations_3 <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
+
+sides_choices <- 2
+entrees_choices <- 1
+drinks_choices <- 1
+
+meal_combinations_2 <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
 
 #How many meal combinations are there if customers can choose from 
 #6 entrees, 3 drinks, and select 3 sides from the current 6 options?
+sides_options <- 6
+entrees_options <- 6
+drinks_options <- 3
+
 sides_choices <- 3
+entrees_choices <- 1
+drinks_choices <- 1
 meal_combinations_3 <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
+
+#Write a function that takes a number of entree choices and returns 
+#the number of meal combinations possible given that number of entree 
+#options, 3 drink choices, and a selection of 2 sides from 6 options.
+
+meal_combinations_function <- function(entrees_options){
+  sides_options <- 6
+  #entrees_options <- 6
+  drinks_options <- 3
+  
+  sides_choices <- 2
+  entrees_choices <- 1
+  drinks_choices <- 1
+  
+  choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
+}
+
+#Use sapply() to apply the function to entree option counts ranging 
+#from 1 to 12.
+
+entrees_options_seq <- seq(1, 12)
+sapply(entrees_options_seq, meal_combinations_function)
+
