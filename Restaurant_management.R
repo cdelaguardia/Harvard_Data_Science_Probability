8379d50440dba756f0fe8dcbb658f86d8cc5c12b
cdelaguardia <cdelaguardia1987@gmail.com>
2023-07-26 13:42:05 -0600

    Calculating possible meal combinations with 3 side choices

--- Restaurant_management.R
+++ Restaurant_management.R
@@ -17,8 +17,17 @@ sides_options <- 6
 entrees_options <- 6
 drinks_options <- 2
 
-meal_combinations <- choose(entrees_options, 1) * choose(sides_options, 2) * choose(drinks_options, 1)
+sides_choices <- 2
+entrees_choices <- 1
+drinks_choices <- 1
+
+meal_combinations <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
 
 #How many combinations are possible if he expands his original special to 3 drink options?
 drinks_options <- 3
-meal_combinations_2 <- choose(entrees_options, 1) * choose(sides_options, 2) * choose(drinks_options, 1)
+meal_combinations_3 <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
+
+#How many meal combinations are there if customers can choose from 
+#6 entrees, 3 drinks, and select 3 sides from the current 6 options?
+sides_choices <- 3
+meal_combinations_3 <- choose(entrees_options, entrees_choices) * choose(sides_options, sides_choices) * choose(drinks_options, drinks_choices)
